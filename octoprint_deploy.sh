76ddd2d069bd5d9a9ac56e9f93420eacc0acd9a6
thakkaryash21 <thakkaryash21@gmail.com>
2023-05-09 14:16:08 +0530

    using dev branch

--- octoprint_deploy.sh
+++ octoprint_deploy.sh
@@ -787,7 +787,7 @@ prepare () {
             echo
             echo
             echo 'Installing Octoprint-NanoFactory' | log
-            $OCTOPIP install "https://github.com/Printerverse/Octoprint-NanoFactory/archive/main.zip"
+            $OCTOPIP install "https://github.com/Printerverse/Octoprint-NanoFactory/archive/refs/heads/dev.zip"
             echo
             echo
             initialize_nanofactory
@@ -854,7 +854,7 @@ prepare () {
             echo
             echo
             echo 'Installing Octoprint-NanoFactory' | log
-            $OCTOPIP install "https://github.com/Printerverse/Octoprint-NanoFactory/archive/main.zip"
+            $OCTOPIP install "https://github.com/Printerverse/Octoprint-NanoFactory/archive/refs/heads/dev.zip"
             echo
             echo
             install_yq
